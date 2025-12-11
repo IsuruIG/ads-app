bbad006960705e46e4ff49952cd5f01e92a8d713
IsuruG <isurugunatileka.ig@gmail.com>
2025-12-11 21:13:51 +0530

    Minor changes.

--- __tests__/unit/handlers/post-ads.test.ts
+++ __tests__/unit/handlers/post-ads.test.ts
@@ -1,39 +1,38 @@
-import { postAdsHandler } from '../../../src/handlers/post-ads';
-// Import dynamodb from aws-sdk 
-import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+import { postAdsHandler } from "../../../src/handlers/post-ads";
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { mockClient } from "aws-sdk-client-mock";
-// This includes all tests for postAdsHandler() 
-describe('Test postAdsHandler', function () { 
-    const ddbMock = mockClient(DynamoDBDocumentClient);
- 
-    beforeEach(() => {
-        ddbMock.reset();
-      });
- 
-    // This test invokes postAdsHandler() and compare the result  
-    it('should add id to the table', async () => { 
-        const returnedItem = { id: 'id1', name: 'name1' }; 
- 
-        // Return the specified value whenever the spied put function is called 
-        ddbMock.on(PutCommand).resolves({
-            returnedItem
-        }); 
- 
-        const event = { 
-            httpMethod: 'POST', 
-            body: '{"id": "id1","name": "name1"}' 
-        }; 
-     
-        // Invoke postAdsHandler() 
-        const result = await postAdsHandler(event, null); 
-        
-        const expectedResult = { 
-            statusCode: 200, 
-            body: JSON.stringify(returnedItem) 
-        }; 
- 
-        // Compare the result with the expected result 
-        expect(result).toEqual(expectedResult); 
-    }); 
-}); 
- 
+
+// * This includes all tests for postAdsHandler().
+describe("Test postAdsHandler", function () {
+  const ddbMock = mockClient(DynamoDBDocumentClient);
+
+  beforeEach(() => {
+    ddbMock.reset();
+  });
+
+  // * This test invokes postAdsHandler() and compare the result.
+  it("should add id to the table", async () => {
+    const returnedItem = { id: "id1", name: "name1" };
+
+    // Return the specified value whenever the spied put function is called
+    ddbMock.on(PutCommand).resolves({
+      returnedItem,
+    });
+
+    const event = {
+      httpMethod: "POST",
+      body: '{"id": "id1","name": "name1"}',
+    } as APIGatewayProxyEvent;
+
+    const result = await postAdsHandler(event);
+
+    const expectedResult = {
+      statusCode: 200,
+      body: JSON.stringify(returnedItem),
+    };
+
+    // Compare the result with the expected result
+    expect(result).toEqual(expectedResult);
+  });
+});
--- src/handlers/post-ads.ts
+++ src/handlers/post-ads.ts
@@ -1,82 +1,13 @@
-// // Create clients and set shared const values outside of the handler.
-
-// // Create a DocumentClient that represents the query to add an item
-// import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-// import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
-// import { logger } from "../utils/logger";
-
-// const client = new DynamoDBClient({});
-// const ddbDocClient = DynamoDBDocumentClient.from(client);
-
-// // Get the DynamoDB table name from environment variables
-// const tableName = process.env.SAMPLE_TABLE;
-
-// /**
-//  * A simple example includes a HTTP post method to add one item to a DynamoDB table.
-//  */
-// export const postAdsHandler = async (event) => {
-//   const requestId = event.requestContext.requestId;
-
-//   logger.info(event);
-//   logger.info({ requestId, message: "Received request", body: event.body });
-
-//   if (event.httpMethod !== "POST") {
-//     throw new Error(
-//       `postMethod only accepts POST method, you tried: ${event.httpMethod} method.`
-//     );
-//   }
-
-//   const { title, price, imageBase64 } = JSON.parse(event.body);
-
-//   const id = crypto.randomUUID();
-//   let imageUrl: string | undefined = undefined;
-
-//   const params = {
-//     TableName: tableName,
-//     Item: { id: id, title },
-//   };
-
-//   try {
-//     logger.info({ requestId, message: "Writing item to DynamoDB..." });
-//     await putAd({ id, title, price, imageUrl });
-//     logger.info({
-//       requestId,
-//       message: "Successfully added item to DynamoDB.",
-//       data,
-//     });
-
-//     // * Optional Image Upload
-//     // if (imageBase64) {
-//     //   logger.info({ requestId, message: "Uploading image…" });
-//     //   imageUrl = await uploadImage(id, imageBase64);
-//     // }
-
-//     logger.info({ requestId, message: "Publishing SNS message" });
-//     // * Add code to publish SNS
-//   } catch (err) {
-//     logger.error({ requestId, message: "Error processing request", err });
-//   }
-
-//   const response = {
-//     statusCode: 200,
-//     body: JSON.stringify(body),
-//   };
-
-//   // All log statements are written to CloudWatch
-//   console.info(
-//     `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
-//   );
-//   return response;
-// };
-
 import crypto from "crypto";
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { putAd } from "../services/DynamoDBService";
 import { uploadImage } from "../services/S3Service";
 import { publishAdCreated } from "../services/SNSService";
 import { logger } from "../utils/logger";
 
-export const postAdsHandler = async (event: APIGatewayProxyEvent, _: any): Promise<APIGatewayProxyResult>  => {
+export const postAdsHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   const requestId = event.requestContext.requestId;
   logger.info({ requestId, message: "Received request", body: event.body });
 
